41811cbbfbb30d132a9ad7ed81c04c0ec9cd15e0
Seiya Kitano <seiyakitano@SeiyanoMacBook-Air.local>
2021-09-09 20:33:08 +0900

    sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '6c57646fa562df4df20da477c5c357691c3510d0e158bd5847c6ed25945ad5e541f0c89b65f4d302116420c914b1182e409a89f178e8a5327ededbb1a620ad45'
